6bd43a18eee662a907dde194a25c073d80d810e4
Frank Rousseau <frank.rousseau@free.fr>
2021-08-15 16:07:27 +0200

    [vue3] Add icon component

---
+++ src/components/widgets/Icon.vue
@@ -0,0 +1,29 @@
+<template>
+<span>
+  <vue-feather :type="name" />
+</span>
+</template>
+
+<script>
+export default {
+  name: 'icon',
+  components: {
+  },
+
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+
+  computed: {
+  },
+
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
